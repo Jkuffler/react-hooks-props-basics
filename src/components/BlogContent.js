7ea5d660fa08bd990ee56887c6235b66c4d3baaa
Jason <jakuffler@gmail.com>
2021-08-10 13:21:15 -0400

    commit tree ed5026b78b47a34bf64907297bfd8b698c12e240

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+  if (props.isPusblished){
+  //hide unpublished content
+  //null will have DOM display zero elements
+  return null;
+} else {
+  //show published content
+  return(
+    <div id="blog-content">
+      <h1>{props.articleText}</h1>
+      <p>{props.minutesToRead} minutes to read</p>
+      </div>
+    );
+  }         
 }
 
 export default BlogContent;
